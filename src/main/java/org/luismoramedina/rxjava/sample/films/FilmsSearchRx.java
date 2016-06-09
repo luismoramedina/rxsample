31d05cafab41623e0010a56ae210e03c12a61d8f
luismoramedina <luismoramedina@gmail.com>
2016-06-09 14:18:18 +0200

    new films rx sample

---
+++ src/main/java/org/luismoramedina/rxjava/sample/films/FilmsSearchRx.java
@@ -0,0 +1,118 @@
+package org.luismoramedina.rxjava.sample.films;
+
+import rx.Observable;
+import rx.functions.Func2;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.luismoramedina.rxjava.util.RxUtils.createAsyncObservable;
+
+/**
+ * This clones imdb search feature:
+ * <ul>
+ *     <li>Search a list of films</li>
+ *     <li>For each film gets (data and reviews)</li>
+ *     <li>System.out...</li>
+ * </ul>
+ *
+ * We use RxJava implementation to handle different streams, you can run in sync or async mode,
+ * check {@link FilmsSearchRx#sync}. And you can change the {@link FilmsSearchRx#DELAY} to check the service
+ * behaviour when the delay grows.
+ *
+ * @author luismoramedina
+ */
+public class FilmsSearchRx {
+
+    public static final int DELAY = 1000;
+    private static boolean sync = false;
+
+    public static void main(String[] args) throws Throwable{
+
+        long start = new Date().getTime();
+        String search = "history";
+
+        Observable<Object> booksStream = Observable.from(searchByTitle(search));
+        Observable<Object> filmDataAndReviewsObservable = booksStream.flatMap(o -> {
+
+            Observable<String> filmDataStream;
+            Observable<String[]> filmReviewsStream;
+
+            if (sync) {
+                filmDataStream = Observable.just(searchFilmData((String) o));
+                filmReviewsStream = Observable.just(searchFilmReviews((String) o));
+            } else {
+                filmDataStream = createAsyncObservable(() -> searchFilmData((String) o));
+                filmReviewsStream = createAsyncObservable(() -> searchFilmReviews((String) o));
+            }
+
+            return Observable.zip(filmDataStream, filmReviewsStream, (Func2<Object, Object, Object>)
+                    (data, reviews) -> new FilmDataAndReviews(((String) data), ((String[]) reviews)));
+        });
+
+
+        List<Object> films = filmDataAndReviewsObservable.toList().toBlocking().single();
+
+        for (Object film : films) {
+            System.out.println("film = " + Arrays.toString(((FilmDataAndReviews) film).getReviews()));
+        }
+
+        System.out.println(new Date().getTime() - start + "... syn: " + sync + " and delay:" + DELAY);
+    }
+
+    private static String[] searchFilmReviews(String film) {
+        try {
+            Thread.sleep(DELAY);
+        } catch (InterruptedException e) {
+        }
+        if (film.startsWith("America")) {
+            return new String[]{"Aggresive", "Bad"};
+        } else {
+            return new String[]{"Good!"};
+        }
+    }
+
+    private static String searchFilmData(String o) {
+        try {
+            Thread.sleep(DELAY);
+        } catch (InterruptedException e) {
+        }
+        if (o.startsWith("America")) {
+            return "AHX";
+        } else {
+            return "AHOV";
+        }
+    }
+
+    private static String[] searchByTitle(String title) throws InterruptedException {
+        Thread.sleep(DELAY);
+        return new String[]{"American history X", "A history of violence"};
+    }
+
+    private static class FilmDataAndReviews {
+        String filmData;
+        String[] reviews;
+
+        public FilmDataAndReviews(String filmData, String[] reviews) {
+            this.filmData = filmData;
+            this.reviews = reviews;
+        }
+
+        @Override
+        public String toString() {
+            return "FilmDataAndReviews{" +
+                    "filmData='" + filmData + '\'' +
+                    ", reviews=" + Arrays.toString(reviews) +
+                    '}';
+        }
+
+        public String getFilmData() {
+            return filmData;
+        }
+
+        public String[] getReviews() {
+            return reviews;
+        }
+    }
+}
