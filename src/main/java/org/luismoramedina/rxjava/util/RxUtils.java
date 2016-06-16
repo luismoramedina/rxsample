45bb8d096eb813ed2b23c172f64dbfc0039085ce
luismoramedina <luismoramedina@gmail.com>
2016-06-16 13:15:49 +0200

    changes schedulers with Thread.start

--- src/main/java/org/luismoramedina/rxjava/util/RxUtils.java
+++ src/main/java/org/luismoramedina/rxjava/util/RxUtils.java
@@ -1,7 +1,6 @@
 package org.luismoramedina.rxjava.util;
 
 import rx.Observable;
-import rx.schedulers.Schedulers;
 
 /**
  *
@@ -11,12 +10,14 @@ public class RxUtils {
 
     public static <T> Observable<T> createAsyncObservable(Command command) {
         return (Observable<T>) Observable.create(subscriber -> {
-            try {
-                subscriber.onNext(command.execute());
-                subscriber.onCompleted();
-            } catch (Exception e) {
-                subscriber.onError(e);
-            }
-        }).subscribeOn(Schedulers.io());
+            new Thread(() -> {
+                try {
+                    subscriber.onNext(command.execute());
+                    subscriber.onCompleted();
+                } catch (Exception e) {
+                    subscriber.onError(e);
+                }
+            }).start();
+        });
     }
 }
